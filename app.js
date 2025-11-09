97d9aafb61a6c59f79b5bb03bf487df35518c588
Shailendra Rai <shailendrarai153@gmail.com>
2025-11-09 11:07:10 +0530

    dp coonect moongose

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 import express from 'express'
-import path from 'path'
+import mongoose from 'mongoose'
 const app = express()
 
+async function main() {
+     try {
+          await mongoose.connect('mongodb+srv://shailendrarai1110:prince%401110@prince.whitp1n.mongodb.net/Student');
+          // console.log("success")
+     } catch (error) {
+          console.log("error connect to database", error)
+     }
+}
 
+main()
 
-app.listen(700 ,()=>{
- console.log("this is start server")
+app.listen(700, () => {
+     console.log("this is start server")
 })
