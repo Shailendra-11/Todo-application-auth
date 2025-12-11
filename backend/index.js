36465d2444304a8c3143529cf19fede1240d4fa7
Shailendra Rai <shailendrarai153@gmail.com>
2025-12-11 15:44:06 +0530

    code change

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use('' ,(req, res)=>{
+   res.send("HIII")
+})
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
